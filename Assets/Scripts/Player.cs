1c02f112b321499c069c49af0d00b0f2a416f96d
Bach <bachpq2003@gmail.com>
2025-01-24 20:12:50 +0200

    Reticle Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
     private GameObject flashLight;
+
+    [SerializeField]
+    private GameObject mainCamera;
 
     private int toolState = 0;
 
@@ -25,6 +29,18 @@ public class Player : MonoBehaviour
             if (toolState == 1){
                 flashLight.SetActive(true);
             }
+        }
+
+
+
+        //if (Input.GetKeyDown())
+    }
+
+    void FixedUpdate(){
+        Vector3 forwardDirection = mainCamera.transform.forward;
+        RaycastHit hit;
+        if  (Physics.Raycast(mainCamera.transform.position, forwardDirection, out hit, 10, LayerMask.GetMask("FishingSpot"))){
+            Debug.Log("akfhoseihfowegfbjowebfvoew");
         }
     }
 }
