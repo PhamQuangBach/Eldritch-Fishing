12f5966b6f78836ac779160580718352b9e8f6be
Bach <bachpq2003@gmail.com>
2025-01-26 13:35:27 +0200

    Moon

--- Assets/Scripts/Moon/Moon.cs
+++ Assets/Scripts/Moon/Moon.cs
@@ -9,6 +9,9 @@ public class Moon : MonoBehaviour
     [SerializeField]
     private Material moonMaterial;
 
+    [SerializeField]
+    private Light pointLight;
+
     void Start()
     {
         
@@ -21,5 +24,7 @@ public class Moon : MonoBehaviour
         transform.eulerAngles = new Vector3(Mathf.Lerp(120, 0, t), 0, 0);
         moonMaterial.SetColor("_Color", Color.Lerp(new Color(0.6f, 0.6f, 0.6f, 1), new Color(0.5f, 0, 0, 1), t));
         RenderSettings.fogColor = Color.Lerp(new Color(0f, 0f, 0f, 1), new Color(0.05f, 0, 0, 1), t);
+        pointLight.color = Color.Lerp(Color.white, Color.red, t);
+        pointLight.intensity = Mathf.Lerp(0, 0.05f, t);
     }
 }
